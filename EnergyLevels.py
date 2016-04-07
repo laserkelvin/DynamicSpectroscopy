4765fe0e4db3e974af683c2a8a45e74ed3ce8684
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2016-04-07 21:06:15 +1000

    New Routines
    New function that will return the total energy of a combination band
    for any number of frequencies and respective quanta.
    
    Biggest change is a new algorithm for calculating vibrational and
    rotational state counts using generators!

--- EnergyLevels.py
+++ EnergyLevels.py
@@ -2,12 +2,21 @@
     of classical models
 """
 
-
 def HarmonicOscillator(Frequency, N):
     """ Calculate the nth energy level
         of a harmonic oscillator
     """
     return Frequency * (N + 0.5)
+
+def CombinationHarmonicOscillator(Frequencies, Quanta):
+    """ Calculates and sums harmonic frequencies, returning the
+        total energy.
+        Frequencies and Quanta are input as lists.
+    """
+    Energy = 0.
+    for index, frequency in enumerate(Frequencies):
+        Energy = Energy + HarmonicOscillator(frequency, Quanta[index])
+    return Energy
 
 def SymmetricTop(A, B, J, K, Origin=0.0):
     """ Calculate energy levels of a symmetric top
@@ -29,7 +38,7 @@ def CalculateVibrationalEnergyLevels(Frequencies, MaximumEnergy):
     EnergyLevels.sort()                                 # sort in ascending order
     return EnergyLevels
 
-def CalculateOblateTopLevels(A, B, MaximumEnergy, Origin=0.):
+def EMaxOblateTopLevels(A, B, MaximumEnergy, Origin=0.):
     RotationalLevels = []
     J = 0
     K = 0
@@ -41,4 +50,18 @@ def CalculateOblateTopLevels(A, B, MaximumEnergy, Origin=0.):
             RotationalLevels = RotationalLevels + (CurrentJK * (2 * J + 1))       # Add 2J + 1 degeneracy
             K = K + 1
         J = J + 1
+    return RotationalLevels
+
+def JMaxOblateTopLevels(A, B, MaxJ, MaximumEnergy, Origin=0.):
+    RotationalLevels = []
+    for J in xrange(MaxJ):
+        CurrentK = []
+        for K in xrange(0, J):
+            CurrentK = []
+            if SymmetricTop(A, B, J, K, Origin) <= MaximumEnergy:
+                CurrentK.append(SymmetricTop(A, B, J, K, Origin))
+                CurrentK = CurrentK * (2 * J + 1)
+            else:
+                pass
+        RotationalLevels = RotationalLevels + CurrentK
     return RotationalLevels
--- StatisticalDynamics.py
+++ StatisticalDynamics.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import matplotlib
 from EnergyLevels import *
+from itertools import combinations, combinations_with_replacement
 matplotlib.style.use('ggplot')
 
 """ Routines for calculating statistical dynamics using
@@ -11,8 +12,7 @@ matplotlib.style.use('ggplot')
     Basic focus for now is on state counting.
 
 """
-
-def StateCountRotatingVibrator(Frequencies, A, B, MaximumEnergy):
+def StateCountRotatingVibrator(Frequencies, A, B, Method="EMax", MaximumEnergy=100., MaximumJ=20):
     """ Combines the harmonic oscillator and symmetric top
         rigid rotor energy levels to give a full state count.
     """
@@ -22,10 +22,54 @@ def StateCountRotatingVibrator(Frequencies, A, B, MaximumEnergy):
         while HarmonicOscillator(frequency, n) <= MaximumEnergy:   # while we're not above maximum energy
             Evib = HarmonicOscillator(frequency, n)
             EnergyLevels.append(Evib)
-            EnergyLevels = EnergyLevels + CalculateOblateTopLevels(A, B, MaximumEnergy, Origin=Evib)
+            if Method == "EMax":
+                EnergyLevels = EnergyLevels + EMaxOblateTopLevels(A, B, MaximumEnergy, Origin=Evib)
+            elif Method == "JMax":
+                EnergyLevels = EnergyLevels + JMaxOblateTopLevels(A, B, MaximumJ, MaximumEnergy, Origin=Evib)
+            else:
+                print " Method error. Specified method does not exist."
+                break
             n = n + 1
     EnergyLevels.sort()
     print " There are a total of \t" + str(len(EnergyLevels)) + "\t states counted."
+    return EnergyLevels
+
+def BruteForceVibrator(Frequencies, MaxQuanta=10, Mode="Run"):
+    """ Calculates and sums harmonic frequencies, returning the
+        total energy.
+        Frequencies are input as a list, and MaxQuanta is an
+        integer.
+    """
+    EnergyLevels = []
+    for length in xrange(len(Frequencies) + 1):
+        # Loop over length number of modes to be considered; i.e. 1 combination, 2 combination etc.
+        LevelGenerator = combinations(Frequencies, length)
+        for Combination in LevelGenerator:
+            # For a given combination of bands, generate a list of quantum numbers
+            if Mode != "Run":
+                print Combination
+            # For a combination of frequencies, we now go through quanta
+            QuantumNumbers = range(0, MaxQuanta)
+            QuantaGenerator = combinations_with_replacement(QuantumNumbers, length)
+            for Quanta in QuantaGenerator:
+                Energy = CombinationHarmonicOscillator(Combination, Quanta)
+                if Mode != "Run":
+                    print Quanta
+                    print Energy
+                EnergyLevels.append(CombinationHarmonicOscillator(Combination, Quanta))
+    print " Total of \t" + str(len(EnergyLevels)) + "\t vibrational states counted."
+    EnergyLevels.sort()
+    return EnergyLevels
+
+def BruteForceRotator(VibrationalEnergies, A, B, MaximumEnergy=1000., MaximumJ=30):
+    """ Supplementary to the BruteForceVibrator; for each vibrational level
+        we tack on rotational levels too.
+    """
+    EnergyLevels = []
+    for Vibration in VibrationalEnergies:
+        EnergyLevels = EnergyLevels + JMaxOblateTopLevels(A, B, MaximumJ, MaximumEnergy, Origin=Vibration)
+    EnergyLevels.sort()
+    print " Total of \t" + str(len(EnergyLevels)) + "\t rovibrational states counted."
     return EnergyLevels
 
 def PlotHistogram(Levels, Bins=100):
